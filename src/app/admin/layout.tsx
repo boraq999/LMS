67b75a567ac4a986b462ad77f36ec28e3477d19b
alhareth alhareth <alharethalalem@gmail.com>
2025-09-10 17:19:37 +0000

    لما sidebar يكون حالة الاغلاق نبيق تعطي للعنص هوا خاصية     align-items:

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -172,7 +172,7 @@ function AdminLayout({ children }: { children: ReactNode }) {
           <SidebarInset>{children}</SidebarInset>
         </div>
          <Sidebar collapsible="icon" variant="sidebar" side="right" className="border-l">
-          <SidebarHeader className="p-4">
+          <SidebarHeader className="p-4 group-data-[collapsible=icon]:items-center">
             <Logo />
           </SidebarHeader>
           <SidebarContent>
--- src/app/student/layout.tsx
+++ src/app/student/layout.tsx
@@ -170,7 +170,7 @@ function StudentLayout({ children }: { children: ReactNode }) {
           <SidebarInset>{children}</SidebarInset>
         </div>
          <Sidebar collapsible="icon" variant="sidebar" side="right" className="border-l">
-          <SidebarHeader className="p-4">
+          <SidebarHeader className="p-4 group-data-[collapsible=icon]:items-center">
             <Logo />
           </SidebarHeader>
           <SidebarContent>
